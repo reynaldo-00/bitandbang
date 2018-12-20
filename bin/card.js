602e3b7085b67fe6c318dafac1b1d7b4590772be
Stublink <chrisrey95@gmail.com>
2018-12-20 12:50:51 -0500

    added codepen and cleaned up code

--- bin/card.js
+++ bin/card.js
@@ -16,15 +16,17 @@ const options = {
 const data = {
   name: chalk.white('Christopher Cedeno /'),
   handle: chalk.cyan('reynld'),
-  work: chalk.white('Software Engineer'),
+  // work: chalk.white('Software Engineer'),
   twitter: chalk.cyan('https://twitter.com/creynl'),
+  codepen: chalk.cyan('https://codepen.io/reynld/'),
   github: chalk.cyan('https://github.com/reynld'),
   linkedin: chalk.cyan('https://linkedin.com/in/reynld'),
   codepen: chalk.cyan('https://codepen.io/reynld/'),
   web: chalk.cyan('https://rey.sh'),
   npx: chalk.white('npx reynld'),
-  labelWork: chalk.white.bold('      Work:'),
+  labelWork: chalk.white.bold('      Software Engineer'),
   labelTwitter: chalk.white.bold('   Twitter:'),
+  labelCodepen: chalk.white.bold('   Codepen:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
   labelWeb: chalk.white.bold('       Web:'),
@@ -34,14 +36,23 @@ const data = {
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
+const working = `${data.labelWork}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
+const codepening = `${data.labelCodepen}  ${data.codepen}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = 
+  heading + newline +  
+  working + newline + newline +
+  githubing + newline + 
+  codepening + newline + 
+  linkedining + newline + 
+  twittering + newline + 
+  webing + newline + newline + 
+  carding
 
 console.log(chalk.green(boxen(output, options)))
